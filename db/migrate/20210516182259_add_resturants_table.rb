c22358a4997b7008170b9ec59ac1b8384b77362f
schmartmann <stefanhartmann@gmail.com>
2021-05-16 14:58:13 -0400

    index for restuarnts

---
+++ db/migrate/20210516182259_add_resturants_table.rb
@@ -0,0 +1,17 @@
+class AddResturantsTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :restaurants, id: :uuid do |t|
+      t.string :name,           null: false
+      t.string :phone,          null: false
+      t.string :email,          null: false
+      t.string :street_address, null: false
+      t.string :city,           null: false
+      t.string :state,          null: false
+      t.string :postal_code,    null: false
+    end
+
+    add_index :restaurants, :email, unique: true
+    add_index :restaurants, :phone, unique: true
+    add_index :restaurants, :name,  unique: true
+  end
+end
