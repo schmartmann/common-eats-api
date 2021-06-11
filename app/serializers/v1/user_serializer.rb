37d4a57916a93363d5dd16aa4ce61c6c9d25b0d1
schmartmann <stefanhartmann@gmail.com>
2021-06-11 16:56:07 -0400

    fix failing users test

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -1,13 +1,5 @@
 module V1
     class UserSerializer < ApplicationSerializer
         attributes :id, :email, :first_name, :last_name, :roles
-
-        def roles
-            {
-                admin: object.admin,
-                customer: object.customer,
-                restauranteur: object.restauranteur
-            }
-        end
     end
 end
